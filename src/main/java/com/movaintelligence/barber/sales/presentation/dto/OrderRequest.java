f029fca3d525b1a3fae8671b09cd15279810d01d
hansen9 <alexanderhansen124@gmail.com>
2025-07-29 10:26:05 +0700

    hapus lombok setter & getter

--- src/main/java/com/movaintelligence/barber/sales/presentation/dto/OrderRequest.java
+++ src/main/java/com/movaintelligence/barber/sales/presentation/dto/OrderRequest.java
@@ -1,11 +1,8 @@
 package com.movaintelligence.barber.sales.presentation.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Getter @Setter
 public class OrderRequest {
     private Long customerId;
     private Long treatmentId;
@@ -13,6 +10,45 @@ public class OrderRequest {
     private boolean isBirthdayDiscount;
     private LocalDateTime orderDate;
 
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getTreatmentId() {
+        return treatmentId;
+    }
+
+    public void setTreatmentId(Long treatmentId) {
+        this.treatmentId = treatmentId;
+    }
+
+    public boolean isRedeem() {
+        return isRedeem;
+    }
+
+    public void setRedeem(boolean redeem) {
+        isRedeem = redeem;
+    }
+
+    public boolean isBirthdayDiscount() {
+        return isBirthdayDiscount;
+    }
+
+    public void setBirthdayDiscount(boolean birthdayDiscount) {
+        isBirthdayDiscount = birthdayDiscount;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
 }
 
 
--- src/main/java/com/movaintelligence/barber/sales/presentation/dto/OrderResponse.java
+++ src/main/java/com/movaintelligence/barber/sales/presentation/dto/OrderResponse.java
@@ -3,13 +3,10 @@ package com.movaintelligence.barber.sales.presentation.dto;
 import com.movaintelligence.barber.crm.domain.entity.Customer;
 import com.movaintelligence.barber.catalog.domain.entity.Treatment;
 import com.movaintelligence.barber.sales.domain.entity.Order;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Getter @Setter
 public class OrderResponse {
     private Long orderId;
     private Long customerId;
@@ -38,6 +35,86 @@ public class OrderResponse {
         order.setRedeemed(isRedeemed);
         order.setBirthdayDiscount(isBirthdayDiscount);
         return order;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Long getTreatmentId() {
+        return treatmentId;
+    }
+
+    public void setTreatmentId(Long treatmentId) {
+        this.treatmentId = treatmentId;
+    }
+
+    public String getTreatmentName() {
+        return treatmentName;
+    }
+
+    public void setTreatmentName(String treatmentName) {
+        this.treatmentName = treatmentName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getAmountPaid() {
+        return amountPaid;
+    }
+
+    public void setAmountPaid(BigDecimal amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public boolean isRedeemed() {
+        return isRedeemed;
+    }
+
+    public void setRedeemed(boolean redeemed) {
+        isRedeemed = redeemed;
+    }
+
+    public boolean isBirthdayDiscount() {
+        return isBirthdayDiscount;
+    }
+
+    public void setBirthdayDiscount(boolean birthdayDiscount) {
+        isBirthdayDiscount = birthdayDiscount;
     }
 }
 
